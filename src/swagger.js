48be37e4dcb1f435cd4cf5b6092c0d1c6b689049
Pradheep P <mantissa6789@gmail.com>
2024-12-09 22:40:17 +0530

    feat(docs) : add Swagger documentation for API Routes using swagger-autogen

---
+++ src/swagger.js
@@ -0,0 +1,22 @@
+import swaggerAutogen from "swagger-autogen";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const outputFile = path.join(__dirname, "swaager.json");
+const endPointFiles = [path.join(__dirname, "routes/index.ts")];
+
+const options = {
+    openapi : "3.0.0",
+    info : {
+        title : "GrowthX backend task",
+        description : "Automated swagger rdocumentation for growthx backend task",
+        version : "1.0.0",
+    },
+};
+
+swaggerAutogen(outputFile, endPointFiles, options).then(() => {
+    console.log("Swagger documentation generated successfully.");
+});
